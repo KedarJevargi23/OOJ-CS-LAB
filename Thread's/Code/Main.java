46663afdf00627bb1087e32f42c8e286f11ab0d0
Kedar Jevargi <kedarjevargi.cs23@bmsce.ac.in>
2024-11-28 23:07:41 +0530

    Add files via upload

---
+++ Thread's/Code/Main.java
@@ -0,0 +1,37 @@
+class CollegeThread extends Thread {
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("BMS College of Engineering");
+                Thread.sleep(10000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("CollegeThread interrupted");
+        }
+    }
+}
+
+class CSEThread extends Thread {
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("CSE");
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("CSEThread interrupted");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        CollegeThread collegeThread = new CollegeThread();
+        CSEThread cseThread = new CSEThread();
+
+        collegeThread.run();
+        cseThread.run();
+    }
+}
